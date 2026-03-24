3b6c6dad8cf524f1f96a90e802bd871756842ccf
Emanuelly <manu.presteslopes10@gmail.com>
2026-03-24 16:07:59 -0300

    model

---
+++ manu09/src/main/java/models/ProjetoModel.java
@@ -0,0 +1,53 @@
+package models;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "tb_projeto")
+public class ProjetoModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    @Column(name = "inicio")
+    private LocalDate dataInicio;
+    @Column(name = "fim")
+    private LocalDate dataFim;
+
+    public ProjetoModel() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public LocalDate getDataInicio() {
+        return dataInicio;
+    }
+
+    public void setDataInicio(LocalDate dataInicio) {
+        this.dataInicio = dataInicio;
+    }
+
+    public LocalDate getDataFim() {
+        return dataFim;
+    }
+
+    public void setDataFim(LocalDate dataFim) {
+        this.dataFim = dataFim;
+    }
+}
