2b3fe8d2ed503f090431c6ac319d9323ab860edf
Emanuelly <manu.presteslopes10@gmail.com>
2026-03-24 16:07:42 -0300

    inicial commit

---
+++ manu09/src/main/java/com/example/manu09/Manu09Application.java
@@ -0,0 +1,13 @@
+package com.example.manu09;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Manu09Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Manu09Application.class, args);
+	}
+
+}
---
+++ manu09/src/test/java/com/example/manu09/Manu09ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.manu09;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Manu09ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
